35ade989b2def3eea78646cd0c00f8d5d9322ac6
Tom <tomhung87@gmail.com>
2016-03-10 14:48:56 -0800

    Add AJAX functions to favoriting posts and adding comments. Comments are now listed by ASC order

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,31 @@
 class CommentsController < ApplicationController
 
 	def create
-		@post = Post.find (params[:post_id])
+		@post = Post.friendly.find (params[:post_id])
 		comment_params = params.require(:comment).permit(:body)
 		@comment = Comment.new comment_params
 		@comment.post = @post
 		@comment.user = current_user
 
-		if @comment.save
-			CommentsMailer.notify_post_owner(@comment).deliver_later
-			redirect_to @comment.post, notice: "Comment made"
-		else 
-			render "posts/show"
+		respond_to do |format|
+			if @comment.save
+				format.html { CommentsMailer.notify_post_owner(@comment).deliver_later
+											redirect_to @comment.post, notice: "Comment made"}
+				format.js { render :create_success }
+			else 
+				format.html { render "posts/show" }
+				format.js		{ render :create_failure } 
+			end
 		end
 	end
 
 	def destroy
-		comment = Comment.find params[:id]
-		comment.destroy
-		redirect_to post_path(params[:post_id]), notice: "Comment deleted"
+		@comment = Comment.find params[:id]
+		@comment.destroy
+		respond_to do |format|
+			format.html { redirect_to post_path(params[:post_id]), notice: "Comment deleted" }
+			format.js { render :delete_success }
+		end
 	end
 
 end
@@ -26,3 +33,10 @@ end
 
 
 
+# if @answer.save
+#       format.html { redirect_to question_path(@question), notice: "Answer created successfully!" }
+#       format.js { render :create_success }
+#     else
+#       format.html { render "questions/show" }
+#       format.js   { render :create_failure }
+#     end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,22 +3,31 @@ class FavoritesController < ApplicationController
 
 
   def create
-    post = Post.find params[:post_id]
+    @post = Post.friendly.find params[:post_id]
     user = current_user
-    favorite = Favorite.new( user: user, post: post)
+    favorite = Favorite.new( user: user, post: @post)
 
-    if favorite.save
-      redirect_to post_path(post), notice: "Thanks for favoriting!"
-    else 
-      redirect_to post_path(post), alert: "You have already favorited"
+    respond_to do |format|
+      if favorite.save
+        format.html { redirect_to post_path(@post), notice: "Thanks for favoriting!" }
+        format.js { render :success_favorite }
+      else 
+        redirect_to post_path(@post), alert: "You have already favorited"
+        format.js { render :fail_favorite }
+      end
     end
   end
 
   def destroy
-    post = Post.find params[:post_id]
+    @post = Post.friendly.find params[:post_id]
     favorite = current_user.favorites.find params[:id]
     favorite.destroy
-    redirect_to post_path(post), notice: "Un-favorited!"
+
+    respond_to do |format|
+      format.html { redirect_to post_path(@post), notice: "Un-favorited!" }  
+      format.js { render }
+    
+    end
   end
   
 end
