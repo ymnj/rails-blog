4093ce663b2ccd2405a8cf0fefb59562074d4afa
Tom <tomhung87@gmail.com>
2016-03-13 21:10:37 -0700

    Fix header background image not displaying on show page. Header is included only in applications views. Center items in the about page. Change root to about page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'home#about'
   get '/about' => 'home#about'
 
 
